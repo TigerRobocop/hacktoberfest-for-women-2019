0c066e0b38d76f76db7140b20c6a40fd65f1673c
Mercia Oliveira <mercia.c.oliveira@accenture.com>
2019-10-30 15:45:07 -0300

    created hello world

---
+++ Scripts/index.js
@@ -0,0 +1,6 @@
+// LANGUAGE: Javascript
+// AUTHOR: Mercia Oliveira
+// GITHUB: https://github.com/MerciaOliveira
+(function () {
+    document.querySelector("[hello-world]").textContent = 'HELLO WORLD';
+})();
