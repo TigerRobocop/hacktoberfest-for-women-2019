4adf40f00ebb593bd4210045bad639af21d73242
Ashish Kumar <54180957+Ashishpurbey@users.noreply.github.com>
2021-10-08 19:03:01 +0530

    Update dummy-do-not-change.js

--- Scripts/dummy-do-not-change.js
+++ Scripts/dummy-do-not-change.js
@@ -2,4 +2,6 @@
 // AUTHOR: Liv Souza
 // GITHUB: https://github.com/TigerRobocop
 
-console.log('Hello World!');
+// console.log('Hello World!');
+// conole.log("Hello world!!!");
+conole.log("Hello world!!!");
